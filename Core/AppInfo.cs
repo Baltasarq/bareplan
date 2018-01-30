24921f9153b78e765d10e1ff18c55a6c4d0fa616
Baltasar <baltasarq@gmail.com>
2018-01-30 14:51:53 +0100

    Updated version info

--- Core/AppInfo.cs
+++ Core/AppInfo.cs
@@ -1,10 +1,10 @@
-// Bareplan (c) 2015-17 MIT License <baltasarq@gmail.com>
+// Bareplan (c) 2015-18 MIT License <baltasarq@gmail.com>
 
 namespace Bareplan.Core {
 	public static class AppInfo {
 		public const string Name = "bareplan";
 		public const string Author = "baltasarq@gmail.com";
-		public const string Version = "1.2 20171201";
+		public const string Version = "1.3 20180130";
 		public const string AppHeader = Name + @" v" + Version;
 	}
 }
